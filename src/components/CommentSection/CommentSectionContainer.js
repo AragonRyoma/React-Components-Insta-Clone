38cd2b84870e52e54840d247e61b19b6c273a6da
Leynier Aragon <aragonleynier@gmail.com>
2020-06-22 21:02:23 -0400

    i think i FINISHED

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -4,12 +4,17 @@ import CommentInput from "./CommentInput";
 import Comment from "./Comment";
 import "./Comment.css";
 
-const CommentSection = props => {
+const CommentSection = (props) => {
   // Add state for the comments
 
+  const comments = props.comments;
+
   return (
+    // map through the comments data and return the Comment component
     <div>
-      {/* map through the comments data and return the Comment component */}
+      {comments.map((comment, i) => (
+        <Comment comment={comment} key={i} />
+      ))}
       <CommentInput />
     </div>
   );
--- src/components/PostsContainer/Post.js
+++ src/components/PostsContainer/Post.js
@@ -7,16 +7,14 @@ import PostHeader from "./PostHeader";
 import "./Posts.css";
 
 // pass props in this file to
-const Post = props => {
+const Post = (props) => {
   // set up state for the likes
 
   return (
     <div className="post-border">
       <PostHeader
         username={props.post.username}
-        thumbnailUrl={
-          props.post.thumbnailUrl
-        }
+        thumbnailUrl={props.post.thumbnailUrl}
       />
       <div className="post-image-wrapper">
         <img
